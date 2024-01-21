5f9d245f45f644f89ec1ad7250074ddb7ebd6ae9
Theodore-Ng <theodore-ng@gmail.com>
2024-01-21 15:15:11 +0700

    tested done offline

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Ultralytics Hub API keys
-URL = "HTTPS://API.ULTRALYTICS.COM/V1/PREDICT/pthFvV9rwxuv8OsvV0aT"
+URL = "https://api.ultralytics.com/v1/predict/PTHfVv9rwxuv8OsvV0aT"
 HEADERS = {"x-api-key": "51d786a5cd3a23433d3c99adb0b4f075aa9173291e"}
 
 # Data
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,24 @@ import streamlit as st
 from PIL import Image
 import requests
 import json
-from config import URL, HEADERS, DATA
+from config import URL, HEADERS
+from config import DATA
 
 # st.set_option("deprecation.showfileUploaderEncoding", False)
 
-st.title("ViT-GPT2 model demo by Tidrael")
+st.title("Brain object detection model demo by Tidrael")
 st.write("")
 
 uploaded_file = st.file_uploader("Upload an image")
 
 if uploaded_file is not None:
     
-    image = Image.open(uploaded_file)
+    # image = Image.open(uploaded_file)
     
     # image_byte = uploaded_file.getvalue()
-    response = requests.post(URL, headers=HEADERS, data=DATA, files={"image": image})
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
     with st.spinner("Loading model, it could take a while..."):
-        response = requests.post(URL, headers=HEADERS, data=DATA, files={"image": image})
-        st.image(image, caption="Uploaded Image.", use_column_width=True)
+        response = requests.post(URL, headers=HEADERS, data=DATA, files={"image": uploaded_file})
     with st.spinner("Predict..."):
         response.raise_for_status()
     st.success(json.dumps(response.json(), indent=2))
