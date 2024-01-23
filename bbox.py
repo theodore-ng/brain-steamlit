b924ed8da7622f85a0c985ce36d31890fb308b49
Theodore-Ng <theodore-ng@gmail.com>
2024-01-23 16:38:36 +0700

    add bbox drawer and results to app

---
+++ bbox.py
@@ -0,0 +1,26 @@
+import cv2
+
+def bbox_convert(results):
+    bboxes = []
+    for result in results:
+        x_min = round(640 * (result["xcenter"] - result["width"] / 2))
+        x_max = round(640 * (result["xcenter"] + result["width"] / 2))
+        y_min = round(640 * (result["ycenter"] - result["height"] / 2))
+        y_max = round(640 * (result["ycenter"] + result["height"] / 2))
+        bbox_dict = {
+            "bbox": [x_min, y_min, x_max, y_max],
+            "label": result["class"]
+        }
+        bboxes.append(bbox_dict)
+    return bboxes
+
+# Draw the bounding box and the label
+def draw_bbox(image, bboxes, path_dir: str):
+    for i in bboxes:
+        bbox = i["bbox"]
+        label = i["label"]
+        cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
+        cv2.putText(image, str(label), (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+    # Save the image
+    cv2.imwrite(path_dir, image)
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,16 @@ import streamlit as st
 from PIL import Image
 import requests
 import json
+import cv2
+from io import StringIO
+import numpy as np
 from config import URL, HEADERS
 from config import DATA
+from bbox import bbox_convert, draw_bbox
 
 # st.set_option("deprecation.showfileUploaderEncoding", False)
 
-st.title("Brain object detection model demo by Tidrael")
+st.title("Brain object detection model demo by Trieu Nguyen")
 st.write("")
 
 uploaded_file = st.file_uploader("Upload an image")
@@ -15,11 +19,25 @@ uploaded_file = st.file_uploader("Upload an image")
 if uploaded_file is not None:
     
     # image = Image.open(uploaded_file)
-    
-    # image_byte = uploaded_file.getvalue()
+
+    # st.write(type(image_byte))
     st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
-    with st.spinner("Loading model, it could take a while..."):
+    
+    # Resquest API
+    with st.spinner("Model is predicting ..."):
         response = requests.post(URL, headers=HEADERS, data=DATA, files={"image": uploaded_file})
-    with st.spinner("Predict..."):
-        response.raise_for_status()
-    st.success(json.dumps(response.json(), indent=2))
+    
+    # Draw result
+    # with st.spinner("Predict..."):
+        # Read the results
+    results = response.json()["data"]
+    bboxes = bbox_convert(results)
+    
+    # Draw bbox
+    image_byte = uploaded_file.getvalue()   # byte like object
+    image_np = np.frombuffer(image_byte, np.uint8)  # convert to numpy array for cv2 read
+    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    draw_bbox(image, bboxes, "bbox.jpg")
+    st.write("Object detect result")
+    st.image("bbox.jpg")
+    st.success("Model runs successfully")
