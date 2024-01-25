16f72c27be176452a82dc3955ef4457ea882853f
Theodore-Ng <theodore-ng@gmail.com>
2024-01-25 09:52:01 +0700

    new API for model YOLOv5s

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Ultralytics Hub API keys
-URL = "https://api.ultralytics.com/v1/predict/PTHfVv9rwxuv8OsvV0aT"
-HEADERS = {"x-api-key": "51d786a5cd3a23433d3c99adb0b4f075aa9173291e"}
+URL = "https://api.ultralytics.com/v1/predict/DWOm8JDoouw2LkWtbXQP"
+HEADERS = {"x-api-key": "https://api.ultralytics.com/v1/predict/DWOm8JDoouw2LkWtbXQP"}
 
 # Data
 DATA = {"size": 640, "confidence": 0.25, "iou": 0.45}
